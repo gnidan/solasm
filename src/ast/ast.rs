5085184adf81e86907aaeb4cb80406e0b00fd7e9
g. nicholas d'andrea <nick@gnidan.org>
2017-04-01 23:11:07 -0400

    Update Item enum options

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -26,6 +26,9 @@ pub enum Item {
     StringLiteral(StringLiteral),
     HexNumber(HexNumber),
     DecNumber(DecNumber),
+    FunctionalExpression(FunctionalExpression),
+    LocalDefinition(LocalDefinition),
+    FunctionalAssignment(FunctionalAssignment),
 }
 
 
