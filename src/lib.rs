94e477de7b1a8742ea85a8f7406347e945d267fd
g. nicholas d'andrea <nick@gnidan.org>
2017-04-08 19:14:00 -0400

    Use process architecture

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,8 @@
 peg_file! __grammar("grammar.rustpeg");
 
 pub mod grammar {
-    pub use super::__grammar::*;
+  pub use super::__grammar::*;
 }
 
 pub mod ast;
-
-pub fn parse(s: &str)
-    -> std::result::Result<ast::Node<ast::Block>, grammar::ParseError > { grammar::block(s) }
+pub mod process;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-use std::io::{self, Read, Write, BufWriter};
+use std::io::{self, Write, BufWriter};
 use std::process;
 
 #[macro_use]
 extern crate clap;
 
 extern crate solasm;
-use solasm::parse;
 use solasm::ast::pretty::PrettyPrinter;
+use solasm::process::Processor;
+use solasm::process::config::{Config, Target};
 
 fn main() {
   let args = clap_app!(solasm =>
@@ -14,14 +15,24 @@ fn main() {
     (author: "g. nicholas d'andrea <nick@gnidan.org>")
     (about: "EVM Assembly Language compiler")
     (@arg ast: --ast "Output formatted assembly")
+    (@arg filename: -f --filename[FILE] "Read from file instead of stdin")
   ).get_matches();
 
-  let output_ast = args.is_present("ast");
+  let mut config = Config::new();
 
-  let mut buffer = String::new();
-  io::stdin().read_to_string(&mut buffer).ok();
+  if args.is_present("ast") {
+    config.target(Target::Assembly);
+  }
 
-  let result = parse(buffer.as_str());
+  if args.is_present("filename") {
+    config.source_file(args.value_of("filename").unwrap());
+  }
+
+  let result = Processor::new()
+    .configure(config.clone())
+    .parse()
+    .parse_result();
+
   if result.is_err() {
     let err = result.err().unwrap();
     println!("Parse Error!\n{}", err);
@@ -31,7 +42,7 @@ fn main() {
   let ast = result.unwrap();
   let mut out : BufWriter<_> = BufWriter::new(io::stdout());
 
-  if output_ast {
+  if config.targets(Target::Assembly) {
     PrettyPrinter::print(&ast, &mut out);
     write!(&mut out, "\n").ok();
   }
