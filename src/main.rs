8ca6006f59e12bc08275f7a983605594e55d425d
g. nicholas d'andrea <nick@gnidan.org>
2017-04-07 22:58:21 -0400

    Add basic parser/formatter bin

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io::{self, Read, Write, BufWriter};
+use std::process;
+
+#[macro_use]
+extern crate clap;
+
+extern crate solasm;
+use solasm::parse;
+use solasm::ast::pretty::PrettyPrinter;
+
+fn main() {
+  let args = clap_app!(solasm =>
+    (version: "0.1.0")
+    (author: "g. nicholas d'andrea <nick@gnidan.org>")
+    (about: "EVM Assembly Language compiler")
+    (@arg ast: --ast  "Output formatted assembly")
+  ).get_matches();
+
+  let output_ast = args.is_present("ast");
+
+  let mut buffer = String::new();
+  io::stdin().read_to_string(&mut buffer).ok();
+
+  let result = parse(buffer.as_str());
+  if result.is_err() {
+    let err = result.err().unwrap();
+    println!("error {}", err);
+    process::exit(1);
+  }
+
+  let ast = result.unwrap();
+  let mut out : BufWriter<_> = BufWriter::new(io::stdout());
+
+  if output_ast {
+    PrettyPrinter::print(&ast, &mut out);
+    write!(&mut out, "\n").ok();
+  }
+}
