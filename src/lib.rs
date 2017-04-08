3f5cbde401d1cf799c0222b246ca3281244a0085
g. nicholas d'andrea <nick@gnidan.org>
2017-04-08 14:47:48 -0400

    Use __grammar internal name for peg_file! mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
 #![feature(plugin)]
 #![plugin(peg_syntax_ext)]
 
-peg_file! grammar("grammar.rustpeg");
+peg_file! __grammar("grammar.rustpeg");
+
+pub mod grammar {
+    pub use super::__grammar::*;
+}
 
 pub mod ast;
 
