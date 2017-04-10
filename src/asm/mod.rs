71746f43bbaca6d15b36af91b73672dc20a64774
g. nicholas d'andrea <nick@gnidan.org>
2017-04-10 19:05:44 -0400

    Move grammar into asm mod

--- src/asm/mod.rs
+++ src/asm/mod.rs
@@ -1,2 +1,8 @@
+peg_file! __grammar("grammar.rustpeg");
+
+pub mod grammar {
+  pub use asm::__grammar::*;
+}
+
 pub mod ast;
 pub mod pretty;
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 #![feature(plugin)]
 #![plugin(peg_syntax_ext)]
 
-peg_file! __grammar("grammar.rustpeg");
-
-pub mod grammar {
-  pub use super::__grammar::*;
-}
-
 pub mod asm;
+pub use asm::grammar;
 pub mod process;
