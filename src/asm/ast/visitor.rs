5088cdbb8b4978c5373d88ba018e7651ec023aa3
g. nicholas d'andrea <nick@gnidan.org>
2017-04-26 02:37:23 -0400

    Add push/pop markers for visitor, walk leaf nodes

--- src/asm/ast/visitor.rs
+++ src/asm/ast/visitor.rs
@@ -1,6 +1,9 @@
 use super::ast::*;
 
 pub trait Visitor<'v>: Sized {
+  fn push(&mut self, _: Nid) {}
+  fn pop(&mut self) {}
+
   fn visit_block(&mut self, b: &'v Node<Block>) {
     &b.walk(self);
   }
@@ -37,8 +40,8 @@ pub trait Visitor<'v>: Sized {
     &f.walk(self);
   }
 
-  fn visit_control_op(&mut self, _: &'v Node<ControlOp>) {
-    // leaf node
+  fn visit_control_op(&mut self, o: &'v Node<ControlOp>) {
+    &o.walk(self);
   }
 
   fn visit_sub_assembly(&mut self, a: &'v Node<SubAssembly>) {
@@ -49,28 +52,28 @@ pub trait Visitor<'v>: Sized {
     &c.walk(self);
   }
 
-  fn visit_identifier(&mut self, _: &'v Node<Identifier>) {
-    // leaf node
+  fn visit_identifier(&mut self, i: &'v Node<Identifier>) {
+    &i.walk(self);
   }
 
   fn visit_literal(&mut self, l: &'v Node<Literal>) {
     &l.walk(self);
   }
 
-  fn visit_string_literal(&mut self, _: &'v Node<StringLiteral>) {
-    // leaf node
+  fn visit_string_literal(&mut self, s: &'v Node<StringLiteral>) {
+    &s.walk(self);
   }
 
-  fn visit_hex_literal(&mut self, _: &'v Node<HexLiteral>) {
-    // leaf node
+  fn visit_hex_literal(&mut self, x: &'v Node<HexLiteral>) {
+    &x.walk(self);
   }
 
-  fn visit_hex_number(&mut self, _: &'v Node<HexNumber>) {
-    // leaf node
+  fn visit_hex_number(&mut self, x: &'v Node<HexNumber>) {
+    &x.walk(self);
   }
 
-  fn visit_dec_number(&mut self, _: &'v Node<DecNumber>) {
-    // leaf node
+  fn visit_dec_number(&mut self, n: &'v Node<DecNumber>) {
+    &n.walk(self);
   }
 }
 
@@ -80,14 +83,17 @@ pub trait Walkable<'w> {
 
 impl<'w> Walkable<'w> for Node<Block> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     for s in &self.statements {
       visitor.visit_statement(s);
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<Statement> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       Statement::Block(ref node) => visitor.visit_block(node),
       Statement::FunctionDefinition(ref node) => visitor.visit_function_definition(node),
@@ -99,21 +105,25 @@ impl<'w> Walkable<'w> for Node<Statement> {
       Statement::ControlOp(ref node) => visitor.visit_control_op(node),
       Statement::SubAssembly(ref node) => visitor.visit_sub_assembly(node),
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<Expression> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       Expression::Identifier(ref node) => visitor.visit_identifier(node),
       Expression::Literal(ref node) => visitor.visit_literal(node),
       Expression::FunctionCall(ref node) => visitor.visit_function_call(node),
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<FunctionDefinition> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       FunctionDefinition { ref identifier, ref arguments, ref returns, ref body } => {
         visitor.visit_identifier(identifier);
@@ -134,11 +144,13 @@ impl<'w> Walkable<'w> for Node<FunctionDefinition> {
         visitor.visit_block(body);
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<VariableDeclaration> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       VariableDeclaration { ref identifiers, ref expression } => {
         for ident in identifiers {
@@ -148,11 +160,13 @@ impl<'w> Walkable<'w> for Node<VariableDeclaration> {
         visitor.visit_expression(expression);
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<Assignment> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       Assignment { ref identifiers, ref expression } => {
         for ident in identifiers {
@@ -162,11 +176,13 @@ impl<'w> Walkable<'w> for Node<Assignment> {
         visitor.visit_expression(expression);
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<Switch> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       Switch { ref expression, ref cases, ref default } => {
         visitor.visit_expression(expression);
@@ -183,22 +199,26 @@ impl<'w> Walkable<'w> for Node<Switch> {
         }
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<Case> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       Case { ref expression, ref block } => {
         visitor.visit_expression(expression);
         visitor.visit_block(block);
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<ForLoop> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       ForLoop { ref init, ref condition, ref post, ref body } => {
         visitor.visit_block(init);
@@ -207,22 +227,26 @@ impl<'w> Walkable<'w> for Node<ForLoop> {
         visitor.visit_block(body);
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<SubAssembly> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       SubAssembly { ref identifier, ref block } => {
         visitor.visit_identifier(identifier);
         visitor.visit_block(block);
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<FunctionCall> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       FunctionCall { ref identifier, ref arguments } => {
         visitor.visit_identifier(identifier);
@@ -231,16 +255,61 @@ impl<'w> Walkable<'w> for Node<FunctionCall> {
         }
       }
     }
+    visitor.pop();
   }
 }
 
 impl<'w> Walkable<'w> for Node<Literal> {
   fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
     match **self {
       Literal::HexNumber(ref node) => visitor.visit_hex_number(node),
       Literal::DecNumber(ref node) => visitor.visit_dec_number(node),
       Literal::StringLiteral(ref node) => visitor.visit_string_literal(node),
       Literal::HexLiteral(ref node) => visitor.visit_hex_literal(node),
     }
+    visitor.pop();
+  }
+}
+
+impl<'w> Walkable<'w> for Node<DecNumber> {
+  fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
+    visitor.pop();
+  }
+}
+
+impl<'w> Walkable<'w> for Node<HexNumber> {
+  fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
+    visitor.pop();
+  }
+}
+
+impl<'w> Walkable<'w> for Node<HexLiteral> {
+  fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
+    visitor.pop();
+  }
+}
+
+impl<'w> Walkable<'w> for Node<StringLiteral> {
+  fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
+    visitor.pop();
+  }
+}
+
+impl<'w> Walkable<'w> for Node<Identifier> {
+  fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
+    visitor.pop();
+  }
+}
+
+impl<'w> Walkable<'w> for Node<ControlOp> {
+  fn walk<V: Visitor<'w>>(&'w self, visitor: &mut V) {
+    visitor.push(self.id);
+    visitor.pop();
   }
 }
